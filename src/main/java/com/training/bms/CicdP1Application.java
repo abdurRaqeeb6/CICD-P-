d3058f8f355fde064ac224d6c9513f464b243886
Abdur Raqeeb M <araqeebm@dhcp-10-120-191-146.wifi.oracle.com>
2024-07-12 09:44:50 +0530

    first commit

---
+++ src/main/java/com/training/bms/CicdP1Application.java
@@ -0,0 +1,14 @@
+package com.training.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdP1Application.class, args);
+		System.out.println("Alstrom app started");
+	}
+
+}
