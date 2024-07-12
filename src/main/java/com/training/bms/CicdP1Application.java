6db6a5ac8c64f2c12b5206ed1ea2b260d5c579d1
Abdur Raqeeb M <araqeebm@dhcp-10-120-191-146.wifi.oracle.com>
2024-07-12 12:17:16 +0530

    First changes

--- src/main/java/com/training/bms/CicdP1Application.java
+++ src/main/java/com/training/bms/CicdP1Application.java
@@ -9,6 +9,7 @@ public class CicdP1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(CicdP1Application.class, args);
 		System.out.println("Alstrom app started");
+		System.out.println("First change");
 	}
 
 }
